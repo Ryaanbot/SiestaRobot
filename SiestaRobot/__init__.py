b8347bffb4f8300c9043ad90a7b6237291ee7010
Ryaanbot <97766892+Ryaanbot@users.noreply.github.com>
2022-02-06 21:16:16 +0700

    Update __init__.py

--- SiestaRobot/__init__.py
+++ SiestaRobot/__init__.py
@@ -38,9 +38,9 @@ logging.getLogger("pyrogram").setLevel(logging.INFO)
 logging.getLogger('ptbcontrib.postgres_persistence.postgrespersistence').setLevel(logging.WARNING)
 
 LOGGER = logging.getLogger('[SiestaRobot]')
-LOGGER.info("Siesta is starting. | An Shiinobu Project Parts. | Licensed under GPLv3.")
+LOGGER.info("Gabut Protection is starting. | Licensed under GPLv3.")
 LOGGER.info("Not affiliated to other anime or Villain in any way whatsoever.")
-LOGGER.info("Project maintained by: github.com/shiinobu (t.me/saint_foire)")
+LOGGER.info("Project maintained by: @gabutjoinsink (t.me/rommelxx)")
 
 # if version < 3.9, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 9:
